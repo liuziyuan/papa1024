c47c5e5971e61d7232a4ab6811dd954fc824bc0a
liuziyuan <liuziyuan.room@gmail.com>
2018-01-15 19:40:28 +0800

    foreach board

--- papa.py
+++ papa.py
@@ -1,6 +1,7 @@
 import requests
 from pyquery import PyQuery as pq
 
+
 def get_index_url(main_page):
     main_doc = pq(main_page.text)
     index_herf = main_doc('a:first').attr('href')
@@ -12,12 +13,19 @@ def get_index_doc(index_url):
     doc = pq(page.text)
     return doc
 
+
 def get_movie_board_links(index_doc):
     movie_table = index_doc('#cate_1')
     board_hash = {0: '', 1: '', 2: '', 4: '', 5: ''}
     for key in board_hash:
-        board_hash[key] = domian_name + movie_table.find("tr").eq(key).find('a').eq(1).attr('href')
+        board_hash[key] = domian_name + \
+            movie_table.find("tr").eq(key).find('a').eq(1).attr('href')
     return board_hash
+
+
+def get_board_posts_links(link):
+    print link
+    return
 
 
 # get main page by domain name
@@ -29,14 +37,8 @@ index_url = get_index_url(main_page)
 index_doc = get_index_doc(domian_name + index_url)
 # get board links which has bt download
 board_links = get_movie_board_links(index_doc)
-# get 
+# get board post list
+for key in board_links:
+    get_board_posts_links(board_links[key])
 
 
-print board_links
-
-
-
-# print type(main_page)
-# print main_page.status_code
-# print main_page.encoding
-# print main_page.cookies
