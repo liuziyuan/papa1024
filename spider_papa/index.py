87934b372429224c834fe9eb08cadc9a8b884eca
liuziyuan <liuziyuan.room@gmail.com>
2018-01-17 16:44:49 +0800

    code refactoring get_url

--- spider_papa/index.py
+++ spider_papa/index.py
@@ -1,7 +1,5 @@
 # -*- coding:utf-8 -*-
 """index module"""
-import requests
-from pyquery import PyQuery as pq
 import spider_papa as papa
 
 def get_doc_by_domian_name(domian_name):
@@ -15,8 +13,7 @@ def get_selected_area(index_doc):
 
 
 def __get_url(domian_name):
-    main_page = requests.get(domian_name)
-    main_doc = pq(main_page.text)
+    main_doc = papa.get_doc(domian_name)
     index_herf = main_doc('a:first').attr('href')
     return index_herf
 
