b79d476263bea8b6f2b6e191cb9d65904a386da5
liuziyuan <liuziyuan.room@gmail.com>
2018-02-05 16:31:12 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='papa1024',
+      version='1.0',
+      description='papa1024 is the web crawler application of the 1024 forum.',
+      author='liuziyuan',
+      author_email='liuziyuan.room@gmail.com',
+      url='https://github.com/liuziyuan/papa1024',
+      install_requires=['requests', 'pyquery', 'threadpool']
+     )
